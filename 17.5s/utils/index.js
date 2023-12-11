edf519e886a51f4d8778c2d6c1fbc6666f4f6ab9
lsoodik <ssonya2311@gmail.com>
2023-12-11 14:10:50 +0700

    os

---
+++ 17.5s/utils/index.js
@@ -0,0 +1,17 @@
+export function definePrime(a) {
+  let isPrime, res;
+  isPrime =true;
+    
+  if ( a >1 && a <=1000){
+    for (let i = 2 ; i < a ; i++){
+      if (a % i ===0){
+        isPrime = false;
+      }
+    }
+    res=isPrime ? `${a} -простое` : `${a} -составное`
+  } else if (a > 1000 || a< 1){
+    res = "данные неверны"
+  }
+  
+  return res;
+}
